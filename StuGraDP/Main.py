c9e51dc2af5a7be36195e3fc8dff3f0dfeb7877f
LukeyHikari <74375351+LukeyHikari@users.noreply.github.com>
2020-12-11 13:32:50 +0800

    Add files via upload

---
+++ StuGraDP/Main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import openpyxl as opyxl 
+from tkinter import *
+
+root = Tk()
+my_label = Label(root, text = "Hello world")
+
+my_label.pack()
+
+root.mainloop()
